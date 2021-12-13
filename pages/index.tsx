17ab4322331b3c582942e8bbafcc76eb07f5a983
Babugia <guibbg12345@gmail.com>
2021-12-13 15:35:08 -0300

    feat: bootstrap details section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,12 @@
-import { Container, Flex, VStack } from '@chakra-ui/react';
+import { Container, Flex } from '@chakra-ui/react';
+import { Cart, Details } from './src/sections';
 
 const IndexPage = () => {
   return (
   <Container maxW="container.xl" padding={0}>
     <Flex h="100vh" py={20}>
-      <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start">
-
-      </VStack>
-
-      <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start" bg="gray.50">
-
-      </VStack>
+      <Details />
+      <Cart />
     </Flex>
   </Container>)
 };
