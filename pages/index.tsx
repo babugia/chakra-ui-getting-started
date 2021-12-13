4d6de90136857212b0bbd12b688ac82dba764162
Babugia <guibbg12345@gmail.com>
2021-12-13 14:59:59 -0300

    feat: create layout using Container, Flex and VStack

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,18 @@
-const IndexPage = () => <h1>Hello Next</h1>;
+import { Container, Flex, VStack } from '@chakra-ui/react';
+
+const IndexPage = () => {
+  return (
+  <Container maxW="container.xl" padding={0}>
+    <Flex h="100vh" py={20}>
+      <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start">
+
+      </VStack>
+
+      <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start" bg="gray.50">
+
+      </VStack>
+    </Flex>
+  </Container>)
+};
 
 export default IndexPage;
